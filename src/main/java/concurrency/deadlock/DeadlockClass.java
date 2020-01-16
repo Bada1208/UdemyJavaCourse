efccf7f46e95d321e8e41d347bdf379ba7e2952a
Bada1208 <sysoievbogdan@gmail.com>
2020-01-16 06:07:53 +0100

    added deadlock package

---
+++ src/main/java/concurrency/deadlock/DeadlockClass.java
@@ -0,0 +1,116 @@
+package concurrency.deadlock;
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DeadlockClass {
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        Thread thread1 = new Thread(() -> runner.firstThread());
+        Thread thread2 = new Thread(() -> runner.secondThread());
+
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        runner.finished();
+    }
+
+}
+
+class Runner {
+    private Account account1 = new Account();
+    private Account account2 = new Account();
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
+    Random random = new Random();
+
+    private void takeLocks(Lock lock1, Lock lock2) {
+        boolean firstLockTaken = false;
+        boolean secondLockTaken = false;
+        while (true) {
+            try {
+                firstLockTaken = lock1.tryLock();
+                secondLockTaken = lock2.tryLock();
+            } finally {
+                if (firstLockTaken && secondLockTaken) return;
+                if (firstLockTaken) lock1.unlock();
+                if (secondLockTaken) lock2.unlock();
+            }
+            Thread.yield();
+        }
+    }
+
+    public void firstThread() {
+       /* for (int i = 0; i < 10000; i++) {
+            synchronized (account1) {
+                synchronized (account2) {
+                    Account.transfer(account1, account2, random.nextInt(100));
+                }
+            }
+        }*/
+        for (int i = 0; i < 10000; i++) {
+            takeLocks(lock1, lock2);
+            try {
+                Account.transfer(account1, account2, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
+        }
+
+    }
+
+    public void secondThread() {
+
+        /*for (int i = 0; i < 10000; i++) {
+            synchronized (account1) {
+                synchronized (account2) {
+                    Account.transfer(account2, account1, random.nextInt(100));
+                }
+            }
+        }*/
+        for (int i = 0; i < 10000; i++) {
+            takeLocks(lock2, lock1);
+            try {
+                Account.transfer(account2, account1, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
+
+        }
+    }
+
+    public void finished() {
+        System.out.println(account1.getBalance());
+        System.out.println(account2.getBalance());
+        System.out.println("Total balance " + (account1.getBalance() + account2.getBalance()));
+    }
+}
+
+class Account {
+    private int balance = 10000;
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public static void transfer(Account acc1, Account acc2, int amount) {
+        acc1.withdraw(amount);
+        acc2.deposit(amount);
+    }
+}
